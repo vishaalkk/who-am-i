e017cea208da03613ace0070b10d245aea9664a9
Vishal Kella <vishal.kella@gmail.com>
2026-03-15 17:00:22 -0400

    content: add For Grace, After A Party by Frank O'Hara

--- src/data/poems.ts
+++ src/data/poems.ts
@@ -1,8 +1,58 @@
 export const poems = [
   {
-    title: "This Be The Verse — Philip Larkin",
+    title: "This Be The Verse",
+    source: "Philip Larkin",
     excerpt: "They fuck you up, your mum and dad.   \n    They may not mean to, but they do.   \nThey fill you with the faults they had\n    And add some extra, just for you.\n\nBut they were fucked up in their turn\n    By fools in old-style hats and coats,   \nWho half the time were soppy-stern\n    And half at one another’s throats.\n\nMan hands on misery to man.\n    It deepens like a coastal shelf.\nGet out as early as you can,\n    And don’t have any kids yourself.",
     type: "Poem",
     link: "https://www.poetryfoundation.org/poems/48419/this-be-the-verse"
+  },
+  {
+    title: "Love after Love",
+    source: "Derek Walcott",
+    excerpt: `The time will come
+when, with elation,
+you will greet yourself arriving
+at your own door, in your own mirror
+and each will smile at the other’s welcome,
+
+and say, sit here. Eat.
+You will love again the stranger who was your self.
+Give wine. Give bread, Give back your heart
+to itself, to the stranger who has loved you
+
+all your life, whom you ignored
+for another, who knows you by heart.
+Take down the love letters from the bookshelf
+
+the photographs, the desperate notes,
+peel your own image from the mirror.
+Sit. Feast on your life.`,
+    type: "Poem"
+  },
+  {
+    title: "For Grace, After A Party",
+    source: "Frank O'Hara",
+    excerpt: `You do not always know what I am feeling.
+Last night in the warm spring air while I was
+blazing my tirade against someone who doesn't
+interest
+        me, it was love for you that set me
+afire,
+
+     and isn't it odd? for in rooms full of
+strangers my most tender feelings
+                                  writhe and
+bear the fruit of screaming. Put out your hand,
+isn't there
+             an ashtray, suddenly, there? beside
+the bed?  And someone you love enters the room
+and says wouldn't
+                  you like the eggs a little
+
+different today?
+                And when they arrive they are
+just plain scrambled eggs and the warm weather
+is holding.`,
+    type: "Poem"
   }
 ];
